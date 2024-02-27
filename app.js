442b4c9260c7f4cccfe944bda223f08fa38f99b6
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-02-27 18:59:31 +0530

    Tasks first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// Unfortunately, directly converting latitude and longitude to a location name (city, state, etc.) is not possible using pure JavaScript without any external APIs. This process, called reverse geocoding, requires accessing geographical databases and translating coordinates into human-readable addresses. JavaScript alone doesn't have this built-in functionality.
+
+
+const button = document.getElementById('get-location-button')
+const buttonForIP = document.getElementById('get-ip-button')
+
+
+//Fetching data from API's
+
+//For Location
+async function getData(lat, long) {
+    const promise = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=6afaaa33cf0449e0a2975348242702&q=${lat},${long}&aqi=yes`
+    )
+    return await promise.json()
+}
+
+
+async function gotlocation(position) {
+    const res = await getData(position.coords.latitude, position.coords.longitude);
+    const locationName = (res.location.name)
+    const locationRegion = (res.location.region)
+    document.getElementById('data').innerHTML = `The Location is ${locationName}, ${locationRegion}`
+}
+
+
+function failedToGetLocation() {
+    console.log("There was some issue to fetch the location");
+}
+
+//For IP address
+async function getIP() {
+    const fetchData = await fetch('https://api.ipify.org/?format=json')
+    const fetchIP = await fetchData.json()
+    document.getElementById('data-ip').innerHTML = `The IP address is ${fetchIP.ip}`
+}
+
+//Display Loaction data on Webpage
+button.addEventListener('click', async () => {
+    navigator.geolocation.getCurrentPosition(gotlocation, failedToGetLocation)
+})
+
+//Display IP address on the webpage
+buttonForIP.addEventListener('click', getIP)
